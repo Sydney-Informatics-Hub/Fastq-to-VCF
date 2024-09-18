fb453b21ba9ac09ad9f6d5f2f89a6f2b481f0419
Cali Willet <cali.willet@sydney.edu.au>
2024-09-18 13:29:23 +1000

    Fastq split cleanup check

--- Scripts/split_fastq_make_input.sh
+++ Scripts/split_fastq_make_input.sh
@@ -30,9 +30,40 @@ outdir=./Fastq_split
 logdir=./Logs/Fastq_split
 fastq_dir=./Fastq
 
+printf "All fastq files in ${fastq_dir} are about to be split and written to ${outdir}\n"
+printf "If you have previously split these fastq to this output directory, you MUST first\n"
+printf "run a cleanup. Fastp does not produce consistent numeric output names and re-splitting\n"
+printf "to the same directory may cause an unintended duplication of some reads.\n"
+printf "\nWould you like to continue? Please enter 'N' if you need to cancel and cleanup manually,\n"
+printf "'C' to run cleanup now (will delete all contents of ${outdir}), or 'Y' to continue without cleanup:\n"
+read response
+
+
+
+if [[ $response == Y ]]
+then 
+	printf "\nContinuing without cleanup\n\n"
+elif [[ $response == N ]]
+then 
+	printf "\nExiting. Please perform manual cleanup then resubmit.\n"
+	exit
+elif [[ $response == C ]]
+then 
+	printf "\nCleaning up ${outdir} then continuing.\n"
+	# Cleanup using a safer way
+	thisdir=${PWD}
+	cd ${outdir}
+	rm -rf *
+	cd ${thisdir} 
+else 
+	printf "\nYour response $response does not match accepted - please use Y, N or C.\n"
+	exit
+fi
+
 mkdir -p $outdir $logdir
 
-pairs=($(ls ${fastq_dir}/*.f*q.gz | sed 's/_R1.*\|_R2.*\|_R1_*\|_R2_*\|.R1.*\|.R2.*//' | uniq))
+#pairs=($(ls ${fastq_dir}/*.f*q.gz | sed 's/_R1\.*\|_R2\.*\|_R1_*\|_R2_*\|\.R1\.*\|\.R2\.*//' | uniq))
+pairs=($(ls ${fastq_dir}/*.f*q.gz | sed 's/_R1\..*\|_R2\..*\|_R1_.*\|_R2_.*\|\.R1.\.*\|\.R2.\.*//' | uniq))
 
 for pair in ${pairs[@]}
 do
